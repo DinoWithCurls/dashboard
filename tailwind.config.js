924b75f063f60e3ea2a5a3aecf762041769efa27
DinoWithCurls <adityarajsingh64@gmail.com>
2022-08-15 21:55:21 +0530

    style: 💄 Added custom colors in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    colors: {
+      'themeblue': '#0D5F9A',
+      'save': '#519BD0',
+      'delete': '#AF0000',
+      'link': '#346BD4',
+      'sectext': '#858585',
+      'revenue': '#DDEFE0',
+      'transaction': '#F4ECDD',
+      'likes': '#EFDADA',
+      'users': '#DEE0EF',
+      'white': '#FFFFFF',
+      'lightgray': '#F5F5F5',
+      'darkgray': '#EDEDED',
+      'bggray': '#E5E5E5',
+    }
   },
   plugins: [],
 }
