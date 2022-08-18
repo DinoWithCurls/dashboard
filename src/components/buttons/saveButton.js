39cbdd20989a3dacadf302b69963982c8b7d6301
DinoWithCurls <adityarajsingh64@gmail.com>
2022-08-18 16:02:36 +0530

    fix: 🐛 Fixed image and save button

--- src/components/buttons/saveButton.js
+++ src/components/buttons/saveButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SaveButton = ({buttonAction}) => {
     return (
-        <button onClick={buttonAction} className='flex justify-center items-center w-1/3 p-2 rounded-lg bg-save text-white font-bold'>
+        <button onClick={buttonAction} className='flex justify-center items-center w-full p-2 rounded-lg bg-save text-white font-bold'>
             Save
         </button>
     )
